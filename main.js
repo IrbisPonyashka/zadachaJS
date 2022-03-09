c05ec9cce64f8dc0dbb245ac3a16601b83052c11
ISmail <ISmail@gmail.com>
2022-03-09 19:53:08 +0500

    js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
-function birth(Name,year,old) {
-    Name = prompt('Введите ваше имя');
-    year = +prompt('Введите ваш год рождения');
-    old = +prompt('Введите нынешний год');
-    let x = old - year;
-    alert('Ваше имя ' + Name + ', вы ' + year + ' года рождения ' + ' и вам ' + x + ' лет ')
-}
-birth()
+// function birth(Name,year,old) {
+//     Name = prompt('Введите ваше имя');
+//     year = +prompt('Введите ваш год рождения');
+//     old = +prompt('Введите нынешний год');
+//     let x = old - year;
+//     alert('Ваше имя ' + Name + ', вы ' + year + ' года рождения ' + ' и вам ' + x + ' лет ')
+// }
+// birth()
 
 function rand(min,max) {
     return Math.floor(Math.random() * (max + 1 - min) + min);
@@ -23,13 +23,13 @@ for(i = 0; i < x;i++) {
         if(answer = (number1 - number2) == primer){
            alert('Ваш ответ верный : ' + primer);
         }else if( alert('Ваш ответ ' + primer + ' не верный. ' + 'Правильный ответ :' + (number1 - number2))); 
-    }else if(number1 < number2){
+    }else if(number1 === 1){
         let primer = +prompt(number1 + ' + ' + number2 + ' =')
         let answer = (number1 + number2);
         if(answer = (number1 + number2) == primer){
             alert('Ваш ответ верный : ' + primer);
          }else if( alert('Ваш ответ ' + primer + ' не верный. ' + ' Правильный ответ :' + (number1 + number2))); 
-    }else if(number1 > 5){
+    }else if(number1 % 2 == 0){
         let primer = +prompt(number1 + ' / ' + number2 + ' =')
         let answer = (number1 / number2) == primer ? 'верный' : 'Ошибка';
         alert(number1 + ' / ' + number2 + ' = ' + (number1 / number2) + ' , ' + primer + ' Ваш ответ ' +   answer);
