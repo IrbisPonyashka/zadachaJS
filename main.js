970c4afd6957079e188afbcdd0298c416e822191
ISmail <ISmail@gmail.com>
2022-03-09 14:55:45 +0500

    js

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 
-function birth(Name,year,old) {
-    Name = prompt('Имя');
-    year = +prompt('Год рождения');
-    old = +prompt('Нынешний год');
-    let x = old - year;
-    alert('Ваше имя ' + Name + ', вы ' + year + ' года рождения ' + ' и вам ' + x + ' лет ')
+// function birth(Name,year,old) {
+//     Name = prompt('Введите ваше имя');
+//     year = +prompt('Введите ваш год рождения');
+//     old = +prompt('Введите нынешний год');
+//     let x = old - year;
+//     alert('Ваше имя ' + Name + ', вы ' + year + ' года рождения ' + ' и вам ' + x + ' лет ')
+// }
+// birth()primer = +prompt(number1 + ' / ' + number2 + ' =')
+
+function rand(min,max) {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
 }
-birth()
-
-
+let x = +prompt('Введите кол-во примеров')
+let minNumber = +prompt('Введите минимальное число'); 
+let maxNumber = +prompt('Введите максимальное число'); 
+for(i = 0; i < x;i++) {
+    let number1 = rand(minNumber,maxNumber) 
+    let number2 = rand(minNumber,maxNumber) 
+    if(number1 > number2){
+        let primer = +prompt(number1 + ' - ' + number2 + ' =')
+        let answer = (number1 - number2);
+        if(answer = (number1 - number2) == primer){
+           alert('Ваш ответ верный : ' + primer);
+        }else if( alert('Ваш ответ ' + primer + ' не верный. ' + 'Правильный ответ :' + (number1 - number2))); 
+    }else if(number1 < number2){
+        let primer = +prompt(number1 + ' + ' + number2 + ' =')
+        let answer = (number1 + number2);
+        if(answer = (number1 + number2) == primer){
+            alert('Ваш ответ верный : ' + primer);
+         }else if( alert('Ваш ответ ' + primer + ' не верный. ' + ' Правильный ответ :' + (number1 + number2))); 
+    }else if(number1 > 5){
+        let primer = +prompt(number1 + ' / ' + number2 + ' =')
+        let answer = (number1 / number2) == primer ? 'верный' : 'Ошибка';
+        alert(number1 + ' / ' + number2 + ' = ' + (number1 / number2) + ' , ' + primer + ' Ваш ответ ' +   answer);
+    }
+    else if(number1 % 2){
+        let primer = +prompt(number1 + ' * ' + number2 + ' =')
+        let answer = (number1 * number2) == primer ? 'верный' : 'Ошибка';
+        alert(number1 + ' * ' + number2 + ' = ' + (number1 * number2) + ' , ' + primer + ' Ваш ответ ' +  answer);
+    }
+}
+// let primer = +prompt(number1 + ' + ' + number2 + ' =');
+//     let answer = (number1 + number2) == primer ? 'Молодец' : 'Ошибка';
