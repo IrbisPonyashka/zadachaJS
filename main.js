3c5174234a1d808cc82c484599950dc1558e0bd7
ISmail <ISmail@gmail.com>
2022-03-16 10:20:42 +0500

    js

--- main.js
+++ main.js
@@ -1,63 +1,20 @@
-// Первое задание
-let proga = {
-    1: {
-        user: 'Пользователь 1',
-        name: prompt('Введите имя', 'Name'),
-        age: +prompt('Введите ваш возраст', ' +18')
-    },
-    2: {
-        user: 'Пользователь 2',
-        name: prompt('Введите имя', 'Name'),
-        age: +prompt('Введите ваш возраст', ' +18')
-    },
-    3: {
-        user: 'Пользователь 3',
-        name: prompt('Введите имя', 'Name'),
-        age: +prompt('Введите ваш возраст', ' +18')
-    },
-    4: {
-        user: 'Пользователь 4',
-        name: prompt('Введите имя', 'Name'),
-        age: +prompt('Введите ваш возраст', ' +18')
-    },
-    5: {
-        user: 'Пользователь 5',
-        name: prompt('Введите имя', 'Name'),
-        age: +prompt('Введите ваш возраст', ' +18')
-    },
-    6: {
-        user: 'Пользователь 6',
-        name: prompt('Введите имя', 'Name'),
-        age: +prompt('Введите ваш возраст', ' +18')
-    },
-    7: {
-        user: 'Пользователь 7 ',
-        name: prompt('Введите имя', 'Name'),
-        age: +prompt('Введите ваш возраст', ' +18')
-    },
-    8: {
-        user: 'Пользователь 8',
-        name: prompt('Введите имя', 'Name'),
-        age: +prompt('Введите ваш возраст', ' +18')
-    },
-    9: {
-        user: 'Пользователь 9 ',
-        name: prompt('Введите имя', 'Name'),
-        age: +prompt('Введите ваш возраст', ' +18')
-    },
-    10: {
-        user: 'Пользователь 10',
-        name: prompt('Введите имя', 'Name'),
-        age: +prompt('Введите ваш возраст', ' +18')
+let ter = [];
+for(let i = 1; i < Infinity; i++){
+    let kod = prompt('Введите команду')
+    let c = kod.split(' ')[0];
+    let v = kod.split(' ')[1];
+    if(c == 'add,'){   
+        ter.push(v);
+    }else if( c == 'del,'){
+        ter = ter.filter((l) => {return l !== v})
+    }else if( kod == 'stop'){
+        break
     }
+    console.log(ter);
 }
-    for(let key in proga){ 
-        for(let newKey in proga[key]){    
-        console.log((proga[key][newKey]));  
-    }
-}
-
-
-
-
-
+    
+    
+    // {
+    //     kod == 'get' ? array.push(kod) : array = [] ;
+    //     kod == 'del' ? array.pop() : array = [] ; 
+    // }
